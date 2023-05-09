95352fcac14ebd29458a47da993cc93bc6a958a9
Andreea-c30 <84787381+Andreea-c30@users.noreply.github.com>
2023-05-10 00:49:49 +0300

    update tests

---
+++ parser/pbl_test.py
@@ -0,0 +1,14 @@
+# Sample module with functions to be tested
+def foo1(param1=1):
+    return param1
+
+def foo2(param2=True, param3='John'):
+    parm=4
+    return 0
+
+def foo3():
+    A=[]
+    res=1.42
+    return res
+
+
